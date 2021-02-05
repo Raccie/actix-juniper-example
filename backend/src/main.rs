a54a240b84fd851051488541a75428d65daf0f9f
raccie <s.baensch@hispeed.ch>
2021-02-05 22:32:21 +0100

    updated actix-related dependencies

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,7 +9,7 @@ use crate::handlers::app_config;
 use actix_cors::Cors;
 use actix_web::{http::header, http::Method, middleware, App, HttpServer};
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let config = Config::from_env().unwrap();
 
@@ -24,8 +24,7 @@ async fn main() -> std::io::Result<()> {
     info!("Starting server at {}", server_url);
 
     HttpServer::new(move || {
-        //let cors = Cors::default()
-        let cors = Cors::new()
+        let cors = Cors::default()
             .allowed_origin(&server_url)
             .allowed_methods(vec![Method::GET, Method::OPTIONS, Method::POST])
             .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
@@ -33,7 +32,7 @@ async fn main() -> std::io::Result<()> {
             .supports_credentials();
 
         App::new()
-            //.wrap(cors)
+            .wrap(cors)
             .wrap(middleware::Logger::default())
             .data(hashing.clone())
             .data(pool.clone())
